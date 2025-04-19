b074a3bbaa96ddbf8c096e733ed2274182be2fea
punctIT <teodorivan2005@gmail.com>
2025-04-19 08:18:06 +0300

    lab 001

---
+++ lab001/probleme/target/debug/deps/probleme.d
@@ -0,0 +1,5 @@
+C:\Users\teodo\Desktop\Rust Laborator\lab001\probleme\target\debug\deps\probleme.exe: src\main.rs
+
+C:\Users\teodo\Desktop\Rust Laborator\lab001\probleme\target\debug\deps\probleme.d: src\main.rs
+
+src\main.rs:
---
+++ lab001/probleme/target/debug/probleme.d
@@ -0,0 +1,1 @@
+C:\Users\teodo\Desktop\Rust\ Laborator\lab001\probleme\target\debug\probleme.exe: C:\Users\teodo\Desktop\Rust\ Laborator\lab001\probleme\src\main.rs
