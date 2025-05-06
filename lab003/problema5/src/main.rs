94b48b6d0aa861ba24be800edc608013e57fb441
punctIT <teodorivan2005@gmail.com>
2025-05-06 14:28:19 +0300

    problema5

--- lab003/problema5/src/main.rs
+++ lab003/problema5/src/main.rs
@@ -1,14 +1,19 @@
 use std::io;
 
 
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 enum ErrorType{
     ZeroDivied,
     NotDigit,
     NotAscii,
     CompuseNumber,
+    Overflow,
+    result,
 }
-fn stringToInt32(s:&String)->Result<i32,ErrorType>{
+fn string_to_int32(s:&String)->Result<i32,ErrorType>{
+    if s.trim() == "result"{
+        return Err(ErrorType::result);
+    }
     let s2 = s.as_bytes();
     for i in 0..s.len(){
         
@@ -30,33 +35,70 @@ fn stringToInt32(s:&String)->Result<i32,ErrorType>{
     }
     return Ok(nr);
 }
+
+fn add_operation(nr1:i32,nr2:i32)-> Result<i32,ErrorType>{
+    match nr1.checked_add(nr2) {
+        Some(sum) => Ok(sum),
+        None => Err(ErrorType::Overflow),
+    }
+}
+fn sub_operation(nr1:i32,nr2:i32)-> Result<i32,ErrorType>{
+    match nr1.checked_sub(nr2) {
+        Some(sum) => Ok(sum),
+        None => Err(ErrorType::Overflow),
+    }
+}
+fn mul_operation(nr1:i32,nr2:i32)-> Result<i32,ErrorType>{
+    match nr1.checked_mul(nr2) {
+        Some(sum) => Ok(sum),
+        None => Err(ErrorType::Overflow),
+    }
+}
+fn div_operation(nr1:i32,nr2:i32)-> Result<i32,ErrorType>{
+    if nr2 == 0 {
+        return Err(ErrorType::ZeroDivied);
+    }
+    match nr1.checked_div(nr2) {
+        Some(sum) => Ok(sum),
+        None => Err(ErrorType::Overflow),
+    }
+}
+
 fn main() {
     let mut nr:i32=0;
     loop {
-        println!("nr: ");
+        println!("number: / 'result'   / Last Result:{}",nr);
         let mut input = String::new();
         io::stdin()
-        .read_line(&mut input)
-        .expect("Eroare la citirea din tastatură");
-
-        /*
-        println!("operatie: ");
-        let mut op = String::new();
-        io::stdin()
-        .read_line(&mut op)
-        .expect("Eroare la citirea din tastatură");
-        if op.trim() == "a" {
-            break;
-        }
-        */
-
+            .read_line(&mut input)
+            .expect("Eroare la citirea din tastatură");
         
-        
-        if stringToInt32(&input).is_ok(){
-            println!("{}",stringToInt32(&input).unwrap());
+        let mut nr_new:i32=0;
+        if string_to_int32(&input).is_ok(){
+            nr_new=string_to_int32(&input).unwrap();
         }
         else {
-            println!("ee");
+            if string_to_int32(&input).err()==Some(ErrorType::result){
+                println!("{}",nr);
+                break;
+            }
+        }
+
+        println!("operatie: + - / %   exit");
+        let mut op = String::new();
+        io::stdin()
+            .read_line(&mut op)
+            .expect("Eroare la citirea din tastatură");
+
+        match op.trim(){
+            "exit"=>break,
+            "+" => if add_operation(nr,nr_new).is_ok() { nr +=nr_new }
+            "-" => if sub_operation(nr,nr_new).is_ok() { nr -=nr_new }
+            "/" => if div_operation(nr,nr_new).is_ok() { nr /=nr_new }
+            "x" => if mul_operation(nr,nr_new).is_ok() { nr *=nr_new }
+            _ => {}
+           
+        
         }
     }
 }
