1f0cd6bd7ec2e9932ee1a2da7c8fb436bd4d00af
punctIT <teodorivan2005@gmail.com>
2025-05-07 11:12:16 +0300

    rez bug

--- algortigm design in rust/lab10 AD/src/main.rs
+++ algortigm design in rust/lab10 AD/src/main.rs
@@ -1,4 +1,4 @@
-
+//10
 fn rabin_karp(t:&String,p:&String)->i32{
     
     fn pow(i:i32,p:usize,q:i32)->i32{
@@ -53,20 +53,21 @@ fn rabin_karp(t:&String,p:&String)->i32{
         }
         if i < t.len() - m {
             y = ((y + q - (t_array[i] * pow(26, m - 1, q)) % q) * 26 + t_array[i + m]) % q;
-            y = (y + q) % q;// in rust modulul poate fi negativ
+            y = (y + q) % q;//  modulul poate fi negativ
         }
     }
 
     return -1;
 }
+
+//14
 #[derive(Debug)]
 enum ErrorType{
     MatrixSizeIncompatibleN,
     MatrixSizeIncompatibleM,
 }
 
-fn matrix_ainb(a:&Vec<Vec<i32>>,b:&Vec<Vec<i32>>)
-                        ->Result<(i32,i32),ErrorType>{
+fn matrix_ainb(a:&Vec<Vec<i32>>,b:&Vec<Vec<i32>>)->Result<(i32,i32),ErrorType>{
     if a.len() > b.len(){
         return Err(ErrorType::MatrixSizeIncompatibleN);
     }
@@ -94,8 +95,14 @@ fn matrix_ainb(a:&Vec<Vec<i32>>,b:&Vec<Vec<i32>>)
     return Ok((-1,-1));
         
 }
-
-fn matrix_rabin_karp(p:&Vec<Vec<char>>,t:&Vec<Vec<char>>)->(usize,usize){
+//15
+fn matrix_rabin_karp(p:&Vec<Vec<char>>,t:&Vec<Vec<char>>)->Result<(i32,i32),ErrorType>{
+    if p.len() > t.len(){
+        return Err(ErrorType::MatrixSizeIncompatibleN);
+    }
+    if p[0].len() > t[0].len(){
+        return Err(ErrorType::MatrixSizeIncompatibleM);
+    }
     let mut t_matrix: Vec<Vec<i32>> = vec![vec![0;t[0].len()];t.len()];
     let mut p_matrix: Vec<Vec<i32>> = vec![vec![0;p[0].len()];p.len()];
     fn make_matrixs(matrix: &mut Vec<Vec<i32>>,s: &Vec<Vec<char>>){
@@ -110,20 +117,68 @@ fn matrix_rabin_karp(p:&Vec<Vec<char>>,t:&Vec<Vec<char>>)->(usize,usize){
             }
         }
     }
-    return (1,1);
+    make_matrixs(& mut t_matrix,t);
+    make_matrixs(& mut p_matrix,p);
+    fn fq(matrix: & Vec<Vec<i32>>,n:usize,m:usize)->i32{
+        let mut result:i32=0;
+        let q=23;
+        for i in 0..n{
+            let mut presult:i32=0;
+            for w in 0..m{
+                presult=(presult*26+ matrix[i][w])%q;
+            }
+            result=(result+presult)%q;
+        }
+        return result;
+    }
+
+    let x=fq(&p_matrix,p.len(),p[0].len());
+    let mut y = fq(&t_matrix,p.len(),p[0].len());
+
+    println!("{} {} ,,,",x,y);
+    for i in 0..=t.len()-p.len(){
+        for j in 0..=t[0].len()-p[0].len(){
+            let mut este:bool=true;
+            if x==y{
+                'iesire: for i2 in 0 .. p.len(){
+                    for j2 in 0..p[0].len(){
+                        if p[i2][j2] != t[i+i2][j+j2] {
+                            este=false;
+                            break 'iesire;
+                        }
+                    }
+                }
+            }
+            if este == true{
+                return Ok((i as i32 , j as i32));
+            }
+            
+
+        }
+    }
+
+    return Ok((-1,-1));
 
 
 }
 
 fn main() {
-    let t:String=String::from("sBsABC");
-    let p:String=String::from("A");
+    println!("rabin karp");
+    let t:String=String::from("AEsABC");
+    let p:String=String::from("AC");
     println!("{}",rabin_karp(&t,&p));
 
+
+
+    println!("\n Matrice In matrice");
     let rows = 3;
     let cols = 4;
-    let matrix = vec![vec![0; cols]; rows];
-    let matrix2 = vec![vec![1; cols-1]; rows-1];
+    let mut matrix = vec![vec![0; cols]; rows];
+    let matrix2 = vec![vec![1; cols-2]; rows-1];
+    matrix[1][1]=1;
+    matrix[1][2]=1;
+    matrix[2][1]=1;
+    matrix[2][2]=1;
     println!("{:?}", matrix);
     println!("{:?}", matrix2);
     if matrix_ainb(&matrix,&matrix2).is_ok(){
@@ -133,10 +188,20 @@ fn main() {
         println!("eroare");
     }
     if matrix_ainb(&matrix2,&matrix).is_ok(){
-        println!("{:?}",matrix_ainb(&matrix2,&matrix2).unwrap());
+        println!("{:?}",matrix_ainb(&matrix2,&matrix).unwrap());
     }
     else {
         println!("eroare");
     }
 
+    println!("\n Rabin Karp-Matrice In matrice");
+    
+    let mut matrixc = vec![vec!['A'; cols]; rows];
+    let matrixc2 = vec![vec!['B'; cols-2]; rows-1];
+    if matrix_rabin_karp(&matrixc2,&matrixc).is_ok(){
+        println!("{:?}",matrix_rabin_karp(&matrixc2,&matrixc).unwrap());
+    }
+    else {
+        println!("eroare");
+    }
 }
